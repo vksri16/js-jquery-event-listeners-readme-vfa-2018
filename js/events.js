a84af9a7fc6d23cb7592d3693a283a0fa3fb2274
Vidisha Srivastav <vidisha.srivastav16@gmail.com>
2018-06-21 17:35:34 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 // call functions here
 getIt()
 frameIt()
+pressIt()
 
 });
 
@@ -22,6 +23,9 @@ function frameIt() {
 
 function pressIt() {
   $('form').on('keydown', function(key) {
-    if(key.which == 71)
+    if(key.which == 71) {
+      alert()
+    }
   })
 }
+
