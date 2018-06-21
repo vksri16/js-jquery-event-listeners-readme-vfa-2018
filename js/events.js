d61373c7c4fed1bdb67260ba0f1bc5f81d20d71c
Vidisha Srivastav <vidisha.srivastav16@gmail.com>
2018-06-21 17:32:35 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,25 @@
 $(document).ready(function(){
 
 // call functions here
+getIt()
+frameIt()
 
 });
+
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if(key.which == )
+  })
+}
